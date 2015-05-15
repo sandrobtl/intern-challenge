3da3e02e4d9c511939fd84e47d3dc27c7f173a4d
Sandro Bertelli <sandro@contaazul.com>
2015-05-15 15:51:48 -0300

    Resolução desafio interno front-end utilizando os padrões do ContaAzul

---
+++ prototype/Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+  pathAssets = 'assets/';
+  pathLess = pathAssets + 'less/';
+  
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+	less : {
+    development: {
+      files: {
+        'assets/css/index.min.css': pathLess + '**/*.less'
+      }
+    }
+  },
+	
+	'http-server': {
+    'dev': {
+      port: 2000,
+      host: '127.0.0.1',
+      showDir : true,
+      autoIndex: true,
+      ext: 'html',
+      runInBackground: true
+	  } 
+  },
+	
+	watch: {
+    styles: {
+      files: pathLess + '**/*.less',
+      tasks: ['less']
+    }
+	}
+ });
+  
+  grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-http-server');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
+	grunt.registerTask('build', ['less']);
+	grunt.registerTask('start', ['http-server','watch']);
+};
